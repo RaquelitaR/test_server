6f8fae2443491777e3157e26d8b6f849654d14f6
raquelitarosaguilar <raquelita15@ru.is>
2017-10-06 22:35:44 +0000

    removing the code and testing one that I found

--- src/httpd.c
+++ src/httpd.c
@@ -1,11 +1,53 @@
+#include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    int len;
+    struct sockaddr_in address;
+    int result;
+    char ch = 'A';
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_port = htons(9734);
+    len = sizeof(address);
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+
+    if (result == -1)
+    {
+        perror("oops: client1");
+        _exit(1);
+    }
+    write(sockfd, &ch, 1);
+    read(sockfd, &ch, 1);
+    printf("char from server = %c\n", ch);
+    close(sockfd);
+    _exit(0);
+}
+
+
+
+
+
+
+
+
+// ----------------------------------------------
+
+/*#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 
 const char *LOG_FILE = "log_file.log";
 int fd_server, fd_client;
@@ -130,7 +172,7 @@ int main(int argc, char *argv[]) {
 
         // Send the message back.
 
-        char response[4096];
+       /* char response[4096];
         memset(response, 0, sizeof(char) * 4096);
         strcat(response, "GET HTTP/1.1\r\nContent-Type: text/html\r\nContent-Length: 35\r\n<html>http://www.raquelita.com ");
         strcat(response, "127.0.0.1:6525");
@@ -154,4 +196,4 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
     }
-}
+}/*
